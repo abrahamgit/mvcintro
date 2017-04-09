9f5e8bf354a6178b6d3a60f6961f2ec452901494
abrahamgit <abraham@360ground.com>
2017-04-09 06:18:08 +0300

    addind the index and the model and the list file.

---
+++ index.php
@@ -0,0 +1,10 @@
+// index.php
+$link = new PDO("mysql:host=localhost;dbname=blog_db", 'myuser', 'mypassword');
+$result = $link->query('SELECT id, title FROM post');
+$posts = array();
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+$posts[] = $row;
+}
+$link = null;
+// include the HTML presentation code
+require 'templates/list.php';
